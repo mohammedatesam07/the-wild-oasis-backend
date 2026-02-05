6ea4176b4946a544363eb76d2f9ade64a4645505
Mohammed Atesam <faizan.mohd.7799110@gmail.com>
2026-02-05 15:05:04 -0500

    authentication complete

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,14 +1,15 @@
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
+import { auth } from "@/app/_lib/auth";
 
 async function Reservation({ cabin }) {
-  console.log(cabin.id);
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
-
+  const session = await auth();
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector
@@ -16,7 +17,11 @@ async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm user={session?.user} cabin={cabin} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
