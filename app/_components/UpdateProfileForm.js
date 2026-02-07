b4eb6859e18cf23b107f2e7ac9bd4d7304d176aa
Mohammed Atesam <faizan.mohd.7799110@gmail.com>
2026-02-06 21:20:58 -0500

    updating reservations

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
 import { useFormState, useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
@@ -57,21 +58,11 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel={"Updating...."}>
+          Update profile
+        </SubmitButton>
       </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { signIn, signOut } from "./auth";
 import supabase from "./supabase";
 import { revalidatePath } from "next/cache";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -14,15 +15,17 @@ export async function updateGuest(formData) {
   if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
     throw new Error("Please provide a valid national ID");
 
-  const updatData = { nationalID, nationality, countryFlag };
+  const updateData = { nationalID, nationality, countryFlag };
   const { data, error } = await supabase
     .from("guests")
-    .update(updatData)
+    .update(updateData)
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
   revalidatePath("/account/profile");
 }
+
+// ***************  Delete Reservation **********************
 
 export async function deleteReservation(bookingId) {
   const session = await auth();
@@ -40,6 +43,36 @@ export async function deleteReservation(bookingId) {
   if (error) throw new Error("Booking could not be deleted");
 
   revalidatePath("/account/reservations");
+}
+
+// ***************  Update Booking **********************
+export async function updateBooking(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in ");
+  const bookingId = Number(formData.get("bookingId"));
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this booking");
+
+  const numGuests = Number(formData.get("numGuests"));
+  const observations = formData.get("observations").slice(0, 1000);
+  const updateData = { numGuests, observations };
+
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  if (error) throw new Error("Booking could not be updated");
+
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath(`/account/reservations`);
+
+  redirect("/account/reservations");
 }
 
 export async function signInAction() {
