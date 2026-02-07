7360d86f8bfa5ea6d57e26ba3e2fd005a684a45b
Mohammed Atesam <faizan.mohd.7799110@gmail.com>
2026-02-06 19:48:02 -0500

    deleteReservation complete

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -86,9 +86,9 @@ export async function getBookings(guestId) {
     .from("bookings")
     // We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
     .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)"
+      "id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestid, cabinid, cabins(name, image)",
     )
-    .eq("guestId", guestId)
+    .eq("guestid", guestId)
     .order("startDate");
 
   if (error) {
@@ -143,7 +143,7 @@ export async function getSettings() {
 export async function getCountries() {
   try {
     const res = await fetch(
-      "https://restcountries.com/v2/all?fields=name,flag"
+      "https://restcountries.com/v2/all?fields=name,flag",
     );
     const countries = await res.json();
     return countries;
